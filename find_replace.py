3d099ac7e2e13cc764b726269e9d8665cabd1db3
Sportsfan77777 <mhammer44444@gmail.com>
2015-09-14 22:49:00 -0700

    small tweaks

--- find_replace.py
+++ find_replace.py
@@ -10,11 +10,12 @@ uses glob to select target files
 import glob
 import fileinput
 
-py_files = glob.glob("runPlanet*.py")
+file_format = "*.*"
+py_files = glob.glob(file_format)
 
-s = ""
+old = ""
+new = ""
 
 for py_file in py_files:
     for line in fileinput.input(py_file, inplace = True):
-        print(line.replace('fine', 
-                      'extend').rstrip())
+        print(line.replace(old, new).rstrip())
